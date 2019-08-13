9638e8722a297e89a32549da106cbf5d688db6ff
Bob Walsh <bob.walsh@47hats.com>
2019-08-13 16:39:54 -0700

    wip

--- public/app.js
+++ public/app.js
@@ -11,8 +11,20 @@ learnjs.showView = function (hash) {
     '#problem': learnjs.problemView
   };
   var hashParts = hash.split('-');
-  var viewFn = routes[hash];
+  var viewFn = routes[hashParts[0]];
   if (viewFn) {
-    $('.view-container').empty().append(viewFn());
+    $('.view-container').empty().append(viewFn(hashParts[1]));
   }
+}
+
+learnjs.problemView = function (problemNumber) {
+  var title = 'Problem #' + problemNumber + ' Coming soon!';
+  return $('<div class="problem-view">').text(title);
+}
+
+learnjs.appOnReady = function () {
+  window.onhashchange = function () {
+    learnjs.showView(window.location.hash);
+  };
+  learnjs.showView(window.location.hash);
 }
